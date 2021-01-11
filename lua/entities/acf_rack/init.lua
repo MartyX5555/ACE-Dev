677909f8431ff53333ffaf9e886351a50511659e
Mr Marty <57878680+MartyX5555@users.noreply.github.com>
2021-01-11 19:21:58 -0300

    guidance return

--- lua/entities/acf_rack/init.lua
+++ lua/entities/acf_rack/init.lua
@@ -842,7 +842,7 @@ function ENT:LoadAmmo( Reload )
     end
     
 	self.NextFire = 0
-	self.PostReloadWait = 0--CurTime() + 4.5
+	self.PostReloadWait = CurTime() --CurTime() + 4.5
     self.WaitFunction = self.GetReloadTime
 
 	self.Ready = false
