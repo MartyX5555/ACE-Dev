62c9347b698fef9ad9739acadb0b1c594ff076b9
Matt <rcheeze1337@gmail.com>
2022-09-24 16:50:44 -0400

    Add function to get armor data for E2 and SF

--- lua/entities/gmod_wire_expression2/core/custom/acffunctions.lua
+++ lua/entities/gmod_wire_expression2/core/custom/acffunctions.lua
@@ -912,35 +912,36 @@ e2function string entity:acfPropMaterial()
 	return (this.ACF.Material or "RHA")
 end
 
---Meant mainly for the armor analyzer. It doesnt work though.
---[[
--- Returns the material of an entity
-e2function table entity:acfPropMaterialData(number Type)
-	if not validPhysics(this) then return {} end
-	if restrictInfo(self, this) then return {} end
-	if not ACF_Check(this) then return {} end
+e2function table entity:acfPropArmorData()
+	local ret = E2Lib.newE2Table()
 
-	local material = this.ACF.Material or "RHA"
-	local MatData = ACE.Armors[material]
-	if not MatData then return {} end
+	if not validPhysics(this) then return ret end
+	if restrictInfo(self, this) then return ret end
+	if not ACF_Check(this) then return ret end
 
-	local curve, effectiveness, massMod
+	local mat = this.ACF.Material
+	if not mat then return ret end
 
-	curve = MatData.curve
-	massMod = MatData.massMod
+	local matData = ACE.Armors[mat]
+	if not matData then return ret end
 
-	if Type > 0 then
+	ret.size = 4
 
-		effectiveness = MatData.HEATeffectiveness or MatData.effectiveness
+	ret.s.Curve = matData.curve
+	ret.stypes.Curve = "n"
 
-	else
+	ret.s.Effectiveness = matData.effectiveness
+	ret.stypes.Effectiveness = "n"
 
-		effectiveness = MatData.effectiveness
-	end
+	ret.s.HEATEffectiveness = matData.HEATEffectiveness or matData.effectiveness
+	ret.stypes.HEATEffectiveness = "n"
 
-	return { Curve = curve , Effectiveness = effectiveness, Material = material }
+	ret.s.Material = mat
+	ret.stypes.Material = "s"
+
+	return ret
 end
-]]
+
 -- [ Fuel Functions ] --
 
 
