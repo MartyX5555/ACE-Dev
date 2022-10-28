305367bd45bb2fb8535acf8b4d85816a165a8ab2
Mr Marty <57878680+MartyX5555@users.noreply.github.com>
2022-10-27 22:26:49 -0300

    Better radio fuse & git 476
    - Improved radio fuze. It will work with any guidance (dumb, radar, infrared, etc). It will detonate by real prop detection around.

--- lua/acf/shared/fuses/d_radio.lua
+++ lua/acf/shared/fuses/d_radio.lua
@@ -21,13 +21,14 @@ this.Target = nil
 this.Distance = 2000
 
 
-this.desc = "This fuse tracks the guidance module's target and detonates when the distance becomes low enough.\nDistance in inches."
+this.desc = "This fuse tracks the guidance module's target and detonates when the distance becomes low enough. The minimum arm delay here is 0.25 secs to avoid undesired results.\nDistance in inches."
 
 
 -- Configuration information for things like acfmenu.
 this.Configurable = table.Copy(this:super().Configurable)
 
 local configs = this.Configurable
+
 configs[#configs + 1] = 
 {
     Name = "Distance",          -- name of the variable to change
@@ -41,29 +42,60 @@ configs[#configs + 1] =
     -- in future if needed: min/max getter function based on munition type.  useful for modifying radar cones?
 }
 
+--We need to clamp the value to 0.25 since many ppl could complain about why their missiles are exploding once fired despite of they have not adjusted a proper arm delay...
+function this:Configure(missile, guidance)
+    self.TimeStarted = CurTime()
+    self.Primer = math.max(self.Primer, 0.25)
+end
 
+do
 
+    local blacklist = {
+        player = true,
+        acf_missile = true,
+        acf_glatgm = true,
+        ace_debris = true,
+        ace_grenade = true,
+        ace_smokegrenade = true,
+        ace_flare = true,
+        ace_antitankmine = true,
+        ace_antipersonelmine = true,
+        ace_boundingmine = true
+    }
 
+    --Question: Should radio fuze be limited to detect props in front of the missile only? Its weird it detonates by detecting something behind it.
+    function this:GetDetonate(missile, guidance)
+        
+        if not self:IsArmed() then return false end
+        
+        local MissilePos = missile:GetPos()
+        local Dist = self.Distance
 
-function this:GetDetonate(missile, guidance)
-	
-    if not self:IsArmed() then return false end
-    
-    local target = guidance.TargetPos or guidance:GetGuidance(missile).TargetPos
-    
-	if not target then return false end
-	if (missile.CurPos + missile.LastVel):DistToSqr(target) > (self.Distance ^ 2) then return false end
-    
-    return true
-    
+        local trace = {}
+        trace.start       = MissilePos
+        trace.endpos      = MissilePos + missile.LastVel * 0.5 --small compensation for incoming impacts.
+        trace.filter      = function(ent) if blacklist[ent:GetClass()] then return false end return true end 
+        trace.mins        = Vector(-Dist, -Dist, -Dist)
+        trace.maxs        = Vector(Dist, Dist, Dist)
+        trace.ignoreworld = true
+
+        local tr = util.TraceHull(trace)
+
+        if tr.Hit then
+
+            debugoverlay.Box(MissilePos, trace.mins, trace.maxs, 1, Color(255,100,0,10))
+            return true
+        end
+
+        return false
+    end
 end
 
 
-
 function this:GetDisplayConfig()
-	return 
-	{
-		["Arming delay"] = math.Round(self.Primer, 3) .. " s",
-		["Distance"] = math.Round(self.Distance / 39.37, 1) .. " m"
-	}
+    return 
+    {
+        ["Arming delay"] = math.Round(self.Primer, 3) .. " s",
+        ["Distance"] = math.Round(self.Distance / 39.37, 1) .. " m"
+    }
 end
