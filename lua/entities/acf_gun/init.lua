1202fff538430ebe649cbfdf71dfec3ced91e587
kerbalcadet <kerbalcadet@gmail.com>
2022-11-29 21:20:00 -0600

    removed unnecessary defs & comments

--- lua/entities/acf_gun/init.lua
+++ lua/entities/acf_gun/init.lua
@@ -857,13 +857,6 @@ do
                 self.CreateShell = ACF.RoundTypes[self.BulletData.Type].create
                 self:CreateShell( self.BulletData )
                 
-                local PhysObj = self:GetPhysicsObject()
-                local HasPhys = not self:GetParent():IsValid()  --No parented
-
-                --nil is due to using applyforcecenter in KEShove function, so masscenter no longer required.
-
-                --local LocalPos = HasPhys and nil or self:GetPos()
-                --local LocalPos = not HasPhys and self:GetPos() or nil
                 local Dir = -self:GetForward()
                 local KE = (self.BulletData.ProjMass * self.BulletData.MuzzleVel * 39.37 + self.BulletData.PropMass * 3500 * 39.37)*(GetConVarNumber("acf_recoilpush") or 1)
 
