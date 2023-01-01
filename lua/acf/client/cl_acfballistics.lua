bbc2a86714dbfd4da23b691baf6ea2825e12d26b
Mr Marty <57878680+MartyX5555@users.noreply.github.com>
2023-01-01 14:32:56 -0300

    what

--- lua/acf/client/cl_acfballistics.lua
+++ lua/acf/client/cl_acfballistics.lua
@@ -15,12 +15,10 @@ function ACF_SimBulletFlight( Bullet, Index )
 
 	if not Bullet or not Index then return end
 
-	--local DeltaTime = ACF.CurTime - Bullet.LastThink
 	local DeltaTime = CurTime() - Bullet.LastThink --intentionally not using cached curtime value
 	
-	local Drag = Bullet.SimFlight:GetNormalized() * (Bullet.DragCoef * Bullet.SimFlight:Length()^2)/ACF.DragDiv
-	--print(Drag)
-	--debugoverlay.Cross(Bullet.SimPos,3,15,Color(255,255,255,32), true)
+	local Drag = Bullet.SimFlight:GetNormalized() * ( Bullet.DragCoef * Bullet.SimFlight:LengthSqr() )/ACF.DragDiv
+
 	Bullet.SimPosLast 	= Bullet.SimPos
 	Bullet.SimPos 		= Bullet.SimPos + (Bullet.SimFlight * ACF.VelScale * DeltaTime)		--Calculates the next shell position
 	Bullet.SimFlight 	= Bullet.SimFlight + (Bullet.Accel - Drag)*DeltaTime			--Calculates the next shell vector
