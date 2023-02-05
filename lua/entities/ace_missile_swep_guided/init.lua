201f4e5af1de33df800eb40809c23807624e4d22
Matt <rcheeze1337@gmail.com>
2023-02-04 19:49:04 -0500

    Javelin/Stinger further improvements
    - General code cleanup
    - Javelin more reliable at hitting targets

--- lua/entities/ace_missile_swep_guided/init.lua
+++ lua/entities/ace_missile_swep_guided/init.lua
@@ -19,29 +19,28 @@ function ENT:Initialize()
 	self.EnergyRetention = 0.95
 	self.MaxTurnRate = 30
 
-	local phys = self:GetPhysicsObject()
-	phys:EnableGravity( true )
-	phys:EnableMotion( true )
-	phys:SetMass(20)
-
 	self.Gravity = 0
-	self.TTime = 50000000
-	self.MissileClimbRatio = self.MissileClimbRatio or 0
+	self.lastTravelTime = 50000000
 
 	self.FuseTime = 10
-	self.phys = phys
 	self.LastTime = CurTime()
+	self.HeightOffset = Vector()
+
+	local phys = self:GetPhysicsObject()
+	self.phys = phys
+
 	if IsValid( phys ) then
+		phys:EnableMotion( true )
+		phys:SetMass(20)
 		phys:Wake()
 		phys:SetBuoyancyRatio( 5 )
 		phys:SetDragCoefficient( 0 )
 		phys:SetDamping( 0, 0 )
 		phys:SetMaterial( "grenade" )
+		phys:EnableGravity(false)
 	end
 
 	self:EmitSound( "acf_extra/airfx/tow2.wav", 100, 100, 2, CHAN_AUTO )
-
-
 end
 
 local function GetRootVelocity(ent)
@@ -61,7 +60,7 @@ function ENT:Detonate()
 	local HEWeight = 4
 	local Radius = HEWeight ^ 0.33 * 8 * 39.37
 
---		ACF_HE( self:GetPos() + Vector(0,0,8) , Vector(0,0,1) , HEWeight , HEWeight*0.5 , self:GetOwner(), nil, self) --0.5 is standard antipersonal mine
+	--ACF_HE( self:GetPos() + Vector(0,0,8) , Vector(0,0,1) , HEWeight , HEWeight*0.5 , self:GetOwner(), nil, self) --0.5 is standard antipersonal mine
 
 	self.FakeCrate = ents.Create("acf_fakecrate2")
 	self.FakeCrate:RegisterTo(self.Bulletdata)
@@ -90,28 +89,33 @@ function ENT:PhysicsCollide()
 end
 
 function ENT:Think()
-
-
 	local curtime = CurTime()
-	self:NextThink( curtime + 0.015 )
 	local DelTime = curtime-self.LastTime
+
+	local pos = self:GetPos()
+
 	self.FuseTime = self.FuseTime - DelTime
 	self.MissileBurnTime = self.MissileBurnTime - DelTime
 	self.LastTime = curtime
-	if not self.LastPos then
-		self.LastPos = self:GetPos()
+
+	if self.IsJavelin and not self.StartDist and IsValid(self.tarent) then
+		local posDiff = (self.tarent:GetPos() - pos)
+		self.StartDist = Vector(posDiff.x, posDiff.y, 0):Length()
 	end
 
-	local tr = util.QuickTrace(self:GetPos() + self:GetForward() * -30, self:GetVelocity() * DelTime * 1.25, {self})
+	if not self.LastPos then
+		self.LastPos = pos
+	end
+
+	local tr = util.QuickTrace(pos + self:GetForward() * -30, self:GetVelocity() * DelTime * 1.25, {self})
 
 	if tr.Hit then
 		debugoverlay.Cross(tr.StartPos, 10, 10, Color(255, 0, 0))
 		debugoverlay.Cross(tr.HitPos, 10, 10, Color(0, 255, 0))
---		self:Detonate()
+
 		self:SetPos(tr.HitPos + self:GetForward() * -18)
 		self.FuseTime = -1
 	else
-		--self:SetPos(NextPos)
 		if self.MissileBurnTime < 0 then
 			if self.MissileBurnTime > -1 then
 				self:StopParticles()
@@ -120,54 +124,53 @@ function ENT:Think()
 			self.MissileThrust = self.MissileThrust * self.EnergyRetention
 			self.Gravity = 9.8 * 39.37 * DelTime
 		end
+
 		self.phys:ApplyForceCenter(20 * (self:GetForward() * self.MissileThrust + Vector(0, 0, -self.Gravity)))
-		debugoverlay.Line(self.LastPos, self:GetPos(), 10, Color(0, 0, 255))
-		self.LastPos = self:GetPos()
+
+		debugoverlay.Line(self.LastPos, pos, 10, Color(0, 0, 255))
+
+		self.LastPos = pos
 	end
 
 
-	if ( IsValid( self.tarent ) )  then
+	if IsValid( self.tarent ) then
+		local posDiff = (self.tarent:GetPos() - pos)
+		local dist = posDiff:Length()
+		local distXY = Vector(posDiff.x, posDiff.y, 0):Length()
+		local travelTime = dist / self:GetVelocity():Length()
+		local TPos = (self.tarent:GetPos() + GetRootVelocity(self.tarent) * travelTime * (self.LeadMul or 1))
 
-		local dist = (self.tarent:GetPos() - self:GetPos()):Length()
-		local TTime = dist / self:GetVelocity():Length()
-		local TPos = (self.tarent:GetPos() + GetRootVelocity(self.tarent) * TTime * (self.LeadMul or 1))
-		local d = (TPos + Vector(0, 0, self.MissileClimbRatio * dist)) - self:GetPos()
-		if dist / (self.StartDist or 0) < 0.5 then
-			self.MissileClimbRatio = 0
+		if self.IsJavelin then
+			-- 5000 = 125 meters or so
+			-- Increase attack angle with distance, or, if within 125 meters, direct attack
+			if self.StartDist > 5000 and math.deg(math.acos(distXY / dist)) < self.StartDist / 8000 then
+				self.HeightOffset = Vector(0, 0, self.StartDist)
+			else
+				self.HeightOffset = Vector()
+			end
 		end
+
+		local d = (TPos + self.HeightOffset) - pos
+
 		if self.RadioDist and d:Length() < self.RadioDist then
 			self.FuseTime = -1
 		end
-		local dadjust = Vector(d.x, d.y, 0)
-		local Range = dadjust:Length()
-		if Range < 4000 then
-			Range = 0
-		end
-		local DTTime = self.TTime - TTime
-		self.TTime = TTime
-	if DTTime > 0 then
-		local AngAdjust = self:WorldToLocalAngles((d):Angle())
-		local adjustedrate = self.MaxTurnRate * DelTime
-		AngAdjust = self:LocalToWorldAngles(Angle(math.Clamp(AngAdjust.pitch, -adjustedrate, adjustedrate), math.Clamp(AngAdjust.yaw, -adjustedrate, adjustedrate), math.Clamp(AngAdjust.roll, -adjustedrate, adjustedrate)))
 
-		self:SetAngles(AngAdjust)
-	else
+		local deltaTravelTime = self.lastTravelTime - travelTime
+		self.lastTravelTime = travelTime
+
+		if deltaTravelTime > 0 then -- Missile is moving away from target
+			local AngAdjust = self:WorldToLocalAngles((d):Angle())
+			local adjustedrate = self.MaxTurnRate * DelTime
+			AngAdjust = self:LocalToWorldAngles(Angle(math.Clamp(AngAdjust.pitch, -adjustedrate, adjustedrate), math.Clamp(AngAdjust.yaw, -adjustedrate, adjustedrate), math.Clamp(AngAdjust.roll, -adjustedrate, adjustedrate)))
+
+			self:SetAngles(AngAdjust)
+		else
 			self.FuseTime = self.FuseTime * 0.5
 		end
---		TarAngle = 		-self.phys:GetAngleVelocity() * 25000000
-
---		TarAngle = self:WorldToLocalAngles( d:Angle() )*1
-
---		Gun:applyAngForce((Gun:toLocal(GunAng) * 250 - Gun:angVel() * 30) * Inertia)
---		local inertia = self.phys:GetInertia()
-
---		self.phys:ApplyTorqueCenter( Vector(TarAngle.pitch,TarAngle.yaw,0) * Vector(inertia.x,inertia.y,inertia.z) )
---		Angle(15,0,0) * self.phys:GetInertia()
---		self:SetAngles(self:LocalToWorldAngles(dir+Angle(Inacc,-Inacc,5)))
 	else
 		self.FuseTime = -1
 	end
-
 
 	if self.FuseTime < 0 then
 		self:Detonate()
