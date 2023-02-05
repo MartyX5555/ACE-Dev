89effa0c086919dbb77f98f24960c2a8787d6977
Mr Marty <57878680+MartyX5555@users.noreply.github.com>
2023-02-05 17:45:28 -0300

    4 tabs

--- lua/entities/ace_scalability/init.lua
+++ lua/entities/ace_scalability/init.lua
@@ -7,131 +7,131 @@ include("shared.lua")
 --[[
 function ENT:Initialize()
 
-    --Use the half value of the final scale lenght. To define the real lenght in the final result 
-    --XYZ scale config should depend on what entity we are scaling, since ammos should scale as shown below, guns by caliber, fuels in the same way as ammo, etc...
+	--Use the half value of the final scale lenght. To define the real lenght in the final result 
+	--XYZ scale config should depend on what entity we are scaling, since ammos should scale as shown below, guns by caliber, fuels in the same way as ammo, etc...
 
-    local Mode = math.random(1,2)
+	local Mode = math.random(1,2)
 
-    local XScale
-    local YScale
-    local ZScale
+	local XScale
+	local YScale
+	local ZScale
 
-    local DefaultSize
-    local ModelPath
+	local DefaultSize
+	local ModelPath
 
-    local EntityScale
+	local EntityScale
 
-    local id
+	local id
 
-    if Mode == 1 then -- This will be used by ammocrates and fueltanks
+	if Mode == 1 then -- This will be used by ammocrates and fueltanks
 
-        id = "models/holograms/rcube_thin.mdl" --This will be given depending on the entity class
+		id = "models/holograms/rcube_thin.mdl" --This will be given depending on the entity class
 
-        XScale = math.random(10,100)
-        YScale = math.random(10,100)
-        ZScale = math.random(10,100)
-    
-        DefaultSize = ACE.ModelData[id].DefaultSize
-        EntityScale = Vector(XScale / DefaultSize, YScale / DefaultSize, ZScale / DefaultSize) 
+		XScale = math.random(10,100)
+		YScale = math.random(10,100)
+		ZScale = math.random(10,100)
+	
+		DefaultSize = ACE.ModelData[id].DefaultSize
+		EntityScale = Vector(XScale / DefaultSize, YScale / DefaultSize, ZScale / DefaultSize) 
 
-        self:SetMaterial("models/props_pipes/GutterMetal01a")
+		self:SetMaterial("models/props_pipes/GutterMetal01a")
 
-    elseif Mode == 2 then -- Adjust size by caliber.
+	elseif Mode == 2 then -- Adjust size by caliber.
 
-        id = "models/tankgun/tankgun_100mm.mdl" --This will be given depending on the entity class
+		id = "models/tankgun/tankgun_100mm.mdl" --This will be given depending on the entity class
 
-        local Caliber = 37--math.random(37,170)
+		local Caliber = 37--math.random(37,170)
 
-        DefaultSize = ACE.ModelData[id].DefaultSize
-        EntityScale = Vector(Caliber / DefaultSize, Caliber / DefaultSize, Caliber / DefaultSize) 
+		DefaultSize = ACE.ModelData[id].DefaultSize
+		EntityScale = Vector(Caliber / DefaultSize, Caliber / DefaultSize, Caliber / DefaultSize) 
 
-    end
+	end
 
-    ModelPath   = ACE.ModelData[id].Model
+	ModelPath   = ACE.ModelData[id].Model
 
-    self:SetModel( ModelPath ) --Make it compatible with ACF-3
-    self:PhysicsInit(SOLID_VPHYSICS)
-    self:SetMoveType( MOVETYPE_VPHYSICS )       
-    self:SetSolid( SOLID_VPHYSICS )
+	self:SetModel( ModelPath ) --Make it compatible with ACF-3
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType( MOVETYPE_VPHYSICS )       
+	self:SetSolid( SOLID_VPHYSICS )
 
-    self.PhysicsObj = self:GetPhysicsObject()
-    self.IsScalable = true
+	self.PhysicsObj = self:GetPhysicsObject()
+	self.IsScalable = true
 
-    do
+	do
 
-        local Phys = self.PhysicsObj
+		local Phys = self.PhysicsObj
 
-        if IsValid(Phys) then
-            Phys:Wake()
-            Phys:SetMass(1000)
+		if IsValid(Phys) then
+			Phys:Wake()
+			Phys:SetMass(1000)
 
-            local Mesh = ACE.ModelData[id].CustomMesh or Phys:GetMeshConvexes()
+			local Mesh = ACE.ModelData[id].CustomMesh or Phys:GetMeshConvexes()
 
-            self.ScaleData = {
-                Mesh = Mesh,
-                Scale = EntityScale,
-                Size = DefaultSize
-            }
+			self.ScaleData = {
+				Mesh = Mesh,
+				Scale = EntityScale,
+				Size = DefaultSize
+			}
 
-            self:ACE_SetScale( self.ScaleData )
+			self:ACE_SetScale( self.ScaleData )
 
-        end 
-    end
+		end 
+	end
 end
 ]]
 do
 
-    local function NetworkNewScale( Ent, Scale )
+	local function NetworkNewScale( Ent, Scale )
 
-        net.Start("ACE_Scalable_Network")
-            net.WriteFloat(Scale.x)
-            net.WriteFloat(Scale.y)
-            net.WriteFloat(Scale.z)
-            net.WriteEntity( Ent )
-        net.Broadcast()
+		net.Start("ACE_Scalable_Network")
+			net.WriteFloat(Scale.x)
+			net.WriteFloat(Scale.y)
+			net.WriteFloat(Scale.z)
+			net.WriteEntity( Ent )
+		net.Broadcast()
 
-    end
+	end
 
-    function ENT:ACE_SetScale( ScaleData )
+	function ENT:ACE_SetScale( ScaleData )
 
-        local MeshData = ScaleData.Mesh
-        local Scale = ScaleData.Scale
-        local Size = ScaleData.Size
-        local PhysMaterial = ScaleData.Material
+		local MeshData = ScaleData.Mesh
+		local Scale = ScaleData.Scale
+		local Size = ScaleData.Size
+		local PhysMaterial = ScaleData.Material
 
-        MeshData = self:ConvertMeshToScale( MeshData, Scale )
+		MeshData = self:ConvertMeshToScale( MeshData, Scale )
 
-        self:PhysicsInitMultiConvex( MeshData )
+		self:PhysicsInitMultiConvex( MeshData )
 		self:SetMoveType(MOVETYPE_VPHYSICS)
 		self:SetSolid(SOLID_VPHYSICS)
 		self:EnableCustomCollisions(true)
 		self:DrawShadow(false)
 
-        self.PhysicsObj = self:GetPhysicsObject()
+		self.PhysicsObj = self:GetPhysicsObject()
 
-        local Phys = self.PhysicsObj
-        if IsValid(Phys) then
-            Phys:Wake()
-            Phys:SetMass(1000)
-            Phys:SetMaterial( PhysMaterial )
-        end 
+		local Phys = self.PhysicsObj
+		if IsValid(Phys) then
+			Phys:Wake()
+			Phys:SetMass(1000)
+			Phys:SetMaterial( PhysMaterial )
+		end 
 
-        NetworkNewScale( self, ScaleData.Scale )
+		NetworkNewScale( self, ScaleData.Scale )
 
-    end
+	end
 
-    net.Receive("ACE_Scalable_Network", function()
+	net.Receive("ACE_Scalable_Network", function()
 
-        local Ent = net.ReadEntity()
+		local Ent = net.ReadEntity()
 
-        if not IsValid(Ent) then return end
-        if not Ent.IsScalable then return end
+		if not IsValid(Ent) then return end
+		if not Ent.IsScalable then return end
 
-        local ScaleData = Ent.ScaleData 
-        
-        NetworkNewScale( Ent, ScaleData.Scale )
+		local ScaleData = Ent.ScaleData 
+		
+		NetworkNewScale( Ent, ScaleData.Scale )
 
-    end)
+	end)
 
 end
 
@@ -142,26 +142,26 @@ do -- AdvDupe2 duped parented ammo workaround
 	-- Only applies for Advanced Duplicator 2
 
 	function ENT:OnDuplicated(EntTable)
-        if self.IsScalable then 
-            local DupeInfo = EntTable.BuildDupeInfo
+		if self.IsScalable then 
+			local DupeInfo = EntTable.BuildDupeInfo
 
-            if DupeInfo and DupeInfo.DupeParentID then
-                self.ParentIndex = DupeInfo.DupeParentID
-    
-                DupeInfo.DupeParentID = nil
-            end
-        end
+			if DupeInfo and DupeInfo.DupeParentID then
+				self.ParentIndex = DupeInfo.DupeParentID
+	
+				DupeInfo.DupeParentID = nil
+			end
+		end
 
 		BaseClass.OnDuplicated(self, EntTable)
 	end
 
 	function ENT:PostEntityPaste(Player, Ent, CreatedEntities)
-        if self.IsScalable then
-            if self.ParentIndex then
-                self.ParentEnt = CreatedEntities[self.ParentIndex]
-                self.ParentIndex = nil
-            end
-        end
+		if self.IsScalable then
+			if self.ParentIndex then
+				self.ParentEnt = CreatedEntities[self.ParentIndex]
+				self.ParentIndex = nil
+			end
+		end
 
 		BaseClass.PostEntityPaste(self, Player, Ent, CreatedEntities)
 	end
